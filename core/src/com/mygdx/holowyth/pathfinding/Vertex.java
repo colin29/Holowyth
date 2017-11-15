2cddeb1345dece9d3a29ba1eaf2238c3ba0ea7fc
Colin Ta <colinta@live.ca>
2017-11-14 19:22:10 -0800

    Forgot to add new class files

---
+++ core/src/com/mygdx/holowyth/pathfinding/Vertex.java
@@ -0,0 +1,6 @@
+package com.mygdx.holowyth.pathfinding;
+
+public class Vertex {
+	public boolean N, S, W, E; //marks whether edge to the to the adjacent vertex is path-able or not.
+	public boolean NW, NE, SW, SE;
+}
